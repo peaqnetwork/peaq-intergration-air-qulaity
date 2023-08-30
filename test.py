af47f6946620c42c646a914ee8c5fc86ffad1132
lavish0000 <lavick100@gmail.com>
2023-08-30 18:25:00 +0530

    added log to check active

--- test.py
+++ test.py
@@ -1,18 +1,16 @@
-import serial
-import time
+from sds011lib import SDS011QueryReader
+from serial import Serial
 
-# Open the serial port connected to the SDS011.
-# Adjust the device name to match your actual device, e.g., '/dev/ttyUSB0'
-ser = serial.Serial('/dev/ttyUSB0', baudrate=9600, stopbits=1, parity="N", timeout=2)
+# Setup a query-mode reader on /dev/ttyUSB0 
+sensor = SDS011QueryReader('/dev/ttyUSB0')
 
-def process_frame(data):
-    pm25 = ((data[3] * 256) + data[2]) / 10.0
-    pm10 = ((data[5] * 256) + data[4]) / 10.0
-    return pm25, pm10
+# Read some data!
+aqi = sensor.query()
+print(aqi.pm25)
+print(aqi.pm10)
 
-while True:
-    data = ser.read(10)
-    if data[0] == 0xAA and data[1] == 0xC0:
-        pm25, pm10 = process_frame(data)
-        print(f"PM2.5: {pm25} μg/m3, PM10: {pm10} μg/m3")
-    time.sleep(1)
+# Put the device to sleep
+sensor.sleep()
+
+# Wake it back up
+sensor.wake()
