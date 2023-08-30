c45284ac91de978ad74576aa31dfe9718aa3bf47
lavish0000 <lavick100@gmail.com>
2023-08-30 17:05:10 +0530

    added log to check active

--- src/AirQualityMonitor.py
+++ src/AirQualityMonitor.py
@@ -12,9 +12,9 @@ class AirQualityMonitor():
     def __init__(self):
         self.sds = SDS011(port='/dev/ttyUSB0')
         self.sds.open('/dev/ttyUSB0')
+        print('SDS011 sensor initialized', self.sds.active)
         # self.sds.mode = 0
         self.sds.period = 1
-        print('SDS011 sensor initialized', self.sds.active)
 
     def get_measurement(self):
         return {
