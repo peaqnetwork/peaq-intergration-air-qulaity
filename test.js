38ed91c42cc29037de5b2e07d95b6f1b5d6c9e50
lavish0000 <lavick100@gmail.com>
2023-08-30 17:35:22 +0530

    added log to check active

--- test.js
+++ test.js
@@ -1,14 +1,26 @@
 const SDS011Wrapper = require("sds011-wrapper");
 
-const main = async () => {
+const main = () => {
   try {
-    const sds011 = new SDS011Wrapper("/dev/ttyUSB0");
+    const sensor = new SDS011Wrapper("/dev/ttyUSB0");
 
-    await sds011.setReportingMode("active");
-    await sds011.setWorkingPeriod(1);
+    sensor
+    .setReportingMode('active')
+    .then(() => {
+        console.log("Sensor is now working in active mode.");
+        return sensor.setWorkingPeriod(0); // Sensor will send data as soon as new data is available.
+    })
+    .then(() => {
+        console.log("Working period set to 0 minutes.");
+        console.log("\nSensor readings:");
 
-    sds011.on("measure", (data) => {
-      console.log(data);
+        // Since working period was set to 0 and mode was set to active, this event will be emitted as soon as new data is received.
+        sensor.on('measure', (data) => {
+            console.log(`[${new Date().toISOString()}] ${JSON.stringify(data)}`);
+        });
+    })
+    .catch((error) => {
+        console.log("errorrrrrrrrrrrrr", error);
     });
   } catch (error) {
     console.log("errorrrrrrrrrrrrr", error);
