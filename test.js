aae75ff4478321d6cbf411ef5824c243ce6e4763
lavish0000 <lavick100@gmail.com>
2023-08-30 17:27:24 +0530

    added log to check active

--- test.js
+++ test.js
@@ -1,10 +1,18 @@
 const SDS011Wrapper = require("sds011-wrapper");
 
-const sds011 = new SDS011Wrapper("/dev/ttyUSB0");
+const main = async () => {
+  try {
+    const sds011 = new SDS011Wrapper("/dev/ttyUSB0");
 
-sds011.setReportingMode("active");
-sds011.setWorkingPeriod(1);
+    await sds011.setReportingMode("active");
+    await sds011.setWorkingPeriod(1);
 
-sds011.on("measure", (data) => {
-    console.log(data);
-});
+    sds011.on("measure", (data) => {
+      console.log(data);
+    });
+  } catch (error) {
+    console.log("errorrrrrrrrrrrrr", error);
+  }
+};
+
+main();
